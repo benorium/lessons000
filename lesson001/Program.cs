2a025a10587d285c35231d2f4d6e533bfa13054f
benorium <benorium@gmail.com>
2022-08-04 21:44:05 +0300

    sqrt number

---
+++ lesson001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("write a number ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int sqrt = number * number;
+// number ** number;
+int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.Write("number in sqrt: " + sqrt1);
+
