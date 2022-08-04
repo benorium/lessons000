a61d1c6fabb5565288339abd394c9bcb9570e185
benorium <benorium@gmail.com>
2022-08-04 22:07:17 +0300

    3 задачи семинар

--- lesson001/Program.cs
+++ lesson001/Program.cs
@@ -1,9 +1,43 @@
 ﻿Console.Clear();
 
-Console.Write("write a number ");
-int number = Convert.ToInt32(Console.ReadLine());
-// int sqrt = number * number;
-// number ** number;
-int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
-Console.Write("number in sqrt: " + sqrt1);
+// Console.Write("write a number ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// // int sqrt = number * number;
+// // number ** number;
+// int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+// Console.Write("number in sqrt: " + sqrt1);
 
+
+
+// Console.Write("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 == num2 *num2)
+// {
+//     Console.Write("First number is a square of second number");
+// }
+// else
+// {
+//     Console.Write("First number isnt a square of second number");
+// }
+
+// Console.Write("Input a number of the day: ");
+
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day < 1 || day > 7)
+// {
+//     Console.Write("Incorrect number");
+// }
+// else
+// {
+//     if (day == 1)
+//     {
+//         Console.Write("Monday");
+//     }
+//     if (day == 2)
+//     {
+//         Console.Write("Tuesday");
+//     }
+// }
