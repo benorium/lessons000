c29f8394492d5d671684596e8c34638445ecc0ac
benorium <benorium@gmail.com>
2022-08-11 17:51:07 +0300

    task007

---
+++ task007/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Input a number of the day: ");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 || day > 7)
+{
+    Console.Write("Incorrect number ");
+}
+else
+{
+    if (day == 1)
+    {
+        Console.Write("no ");
+    }
+    if (day == 2)
+    {
+        Console.Write("no ");
+    }
+    if (day == 3)
+    {
+        Console.Write("no ");
+    }
+    if (day == 4)
+    {
+        Console.Write("no ");
+    }
+    if (day == 5)
+    {
+        Console.Write("no ");
+    }
+    if (day == 6)
+    {
+        Console.Write("yes ");
+    }
+    if (day == 7)
+    {
+        Console.Write("yes ");
+    }
+}
