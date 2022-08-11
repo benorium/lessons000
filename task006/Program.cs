2a740e21ae66fc9c6f5697c7a9e202d4913ae111
benorium <benorium@gmail.com>
2022-08-11 17:39:29 +0300

    task006

---
+++ task006/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter a number : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n > 99 ? n.ToString()[2] : '-');
+Console.ReadKey(true);
+
+
