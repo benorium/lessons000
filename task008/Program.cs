e1957b4e6471e73acb7d6555b55672496812a607
benorium <benorium@gmail.com>
2022-08-17 22:50:46 +0300

    task008

---
+++ task008/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Enter number 5 digit number : ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"your number : {number} - palindrome.");
+  }
+  else Console.WriteLine($"your number : {number} - not a palindrome.");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"enter a correct number");
