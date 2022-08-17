e38325bd03fe82980f9a1f428f0e19b4a030d9b9
benorium <benorium@gmail.com>
2022-08-17 23:04:18 +0300

    task010

---
+++ task010/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от.
+
+Console.Write("enter a number : ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArr(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arr = new int[cube+1];
+Cube(arr);
+PrintArr(arr);
