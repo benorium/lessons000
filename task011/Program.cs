d71b68c7918ccd57980d912442b1e2213de2bcca
benorium <benorium@gmail.com>
2022-08-17 23:20:05 +0300

    task011

---
+++ task011/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Enter first number :");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number :");
+int b = Convert.ToInt32(Console.ReadLine());
+int grade = a;
+
+for (int i = 1; i < b; i++)
+{
+grade = grade * a;
+}
+Console.WriteLine($"{a} in grade {b} = " + grade);
