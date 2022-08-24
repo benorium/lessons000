541384fc475397f2babbfcaeb7031f379f9337b3
benorium <benorium@gmail.com>
2022-08-24 15:20:38 +0300

    task017

---
+++ task017/Program.cs
@@ -0,0 +1,45 @@
+﻿
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0 : {count}");
+
+// Console.Write($"Введи число М(количество чисел): ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] massiveNumbers = new int[m];
+
+// void InputNumbers(int m){
+// for (int i = 0; i < m; i++)
+//   {
+//     Console.Write($"Введи {i+1} число: ");
+//     massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+//   }
+// }
+
+
+// int Comparison(int[] massiveNumbers)
+// {
+//   int count = 0;
+//   for (int i = 0; i < massiveNumbers.Length; i++)
+//   {
+//     if(massiveNumbers[i] > 0 ) count += 1; 
+//   }
+//   return count;
+// }
+
+// InputNumbers(m);
+
+// Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
+
